67c15bf9f3985b7fe2b8f1133906646f1bef0f74
Tiago Dinis <tiagodinis33@gmail.com>
2026-07-28 04:21:17 +0100

    change default language to english

--- radiant-frontend/src/lib/i18n.ts
+++ radiant-frontend/src/lib/i18n.ts
@@ -4,7 +4,7 @@ export const locales = ["pt", "en"] as const
 
 export type AppLocale = (typeof locales)[number]
 
-export const defaultLocale: AppLocale = "pt"
+export const defaultLocale: AppLocale = "en"
 
 export function isLocale(value: string): value is AppLocale {
 	return locales.includes(value as AppLocale)
